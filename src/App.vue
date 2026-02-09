aeff3ab63ab32519c72b61eff6d0fef448bb64b8
linuxie <linuxie@home.com>
2026-02-09 21:50:28 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <h1>🎂 Birthday Message Service</h1>
+    <p>Vue + GitHub Pages + Firebase</p>
+  </div>
+</template>
+
+<style>
+.container {
+  text-align: center;
+  margin-top: 100px;
+  font-family: Arial;
+}
+</style>
